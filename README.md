10a5910f6130f69ffdedd132349dba412974e01b
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-10 22:37:57 -0500

    copy of tech new website for starter code

---
+++ README.md
@@ -0,0 +1,40 @@
+# starter-template
+
+Authored by Charissa Hollister xx/xx/xxxx
+
+## Description
+
+List notes and overall site features here<br />
+
+## Features
+
+List notes and features here<br />
+
+
+## Website Access
+
+### URL Live Webpage:
+
+### GitHub Repo:
+
+![ScreenShot](relative link to image)
+
+## Future improvement opportunities
+
+List them here<br />
+
+### Minimum customer criteria
+
+List them here<br />
+
+To succeed in this Challenge, you’ll apply the following skills:
+
+Structure your application following the Model-View-Controller (MVC) paradigm.
+
+Modularize your code into separate folders for your Models, View, and Controllers to enforce separation of concerns.
+
+Render dynamic HTML for your views using the Handlebars.js template engine.
+
+Implement user authentication.
+
+Configure Heroku so that you can deploy your application using a MySQL database.
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//code to use express-session and sequelize store
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// turn on routes
+app.use(routes);
+
+//code to set up Handlebars.js as your app's template engine of choice
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// turn on connection to db and server
+//!note: change force to true to reset tables
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
