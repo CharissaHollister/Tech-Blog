a4485fce8a2d176ca492d3847d3d97316c1eb424
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-17 18:44:03 -0500

    update login to use only username and password, no email

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,14 +1,14 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const email = document.querySelector("#email-login").value.trim();
+  const username = document.querySelector("#username-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
-  if (email && password) {
+  if (username && password) {
     const response = await fetch("/api/users/login", {
       method: "post",
       body: JSON.stringify({
-        email,
+        username,
         password,
       }),
       headers: { "Content-Type": "application/json" },
@@ -17,7 +17,7 @@ async function loginFormHandler(event) {
     if (response.ok) {
       document.location.replace("/dashboard/");
     } else {
-      alert("Login error: User name or Password invalid");
+      alert("Login error: username or password invalid");
     }
   }
 }
@@ -26,15 +26,13 @@ async function signupFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
-  if (username && email && password) {
+  if (username && password) {
     const response = await fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
         username,
-        email,
         password,
       }),
       headers: { "Content-Type": "application/json" },
